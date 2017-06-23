c84d2999b1f92efafa0a0eb4f85da8e8321ace7a
Xylem-Gaming <xylemgaming.game@gmail.com>
2017-06-23 15:56:52 +0100

    Turned link into API

--- API/link.lua
+++ API/link.lua
@@ -2,9 +2,13 @@ local component = require("component")
 local event = require("event")
 local modem = component.modem
 
+link ={}
+
 m.open(123)
 
 modem.broadcast(321, "This is a test")
 
 local _, _, from, port, _, message = event.pull("modem_message")
 print("Got a message from ".. from .. " on port " .. port .. ": " .. tostring(message))
+
+return link
