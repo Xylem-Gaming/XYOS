0d245b36ac72507ca231d5242389c383e3aa05a0
AceWD <shalev.goo@gmail.com>
2017-06-23 13:23:21 +0300

    AceWD-Patch Link v1

---
+++ API/link.lua
@@ -0,0 +1,10 @@
+local component = require("component")
+local event = require("event")
+local modem = component.modem
+
+m.open(123)
+
+modem.broadcast(321, "This is a test")
+
+local _, _, from, port, _, message = event.pull("modem_message")
+print("Got a message from ".. from .. " on port " .. port .. ": " .. tostring(message))
